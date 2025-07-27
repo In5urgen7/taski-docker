122bcaf72fe4b45551c63aade7ee22bebca04c95
Vyacheslav Maltcev <masaomi0175@gmail.com>
2025-07-27 15:23:22 +0300

    Add actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['51.250.105.246', '127.0.0.1', 'localhost', 'taski-testproject.zapto.org']
 
 
 # Application definition
